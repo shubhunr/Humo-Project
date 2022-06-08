19c9d77d3b7369af038b9fd56a39d2aa38710517
shubhunr <rajshubham160900@gmail.com>
2022-06-08 18:00:55 +0530

    GALLERY Done

--- src/script.js
+++ src/script.js
@@ -1,5 +1,75 @@
-const parentContainer1 =  document.querySelector('.read-more-container');
+const parentContainer1 =  document.querySelector('.read-more-c');
 parentContainer1.addEventListener('click', event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read less..." : "Read More...";
+})
+const parentContainer2 =  document.querySelector('.read-more-co');
+parentContainer2.addEventListener('click', event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read less..." : "Read More...";
+})
+const parentContainer3 =  document.querySelector('.read-more-con');
+parentContainer3.addEventListener('click', event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read less..." : "Read More...";
+})
+const parentContainer4 =  document.querySelector('.read-more-cont');
+parentContainer4.addEventListener('click', event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read less..." : "Read More...";
+})
+const parentContainer5 =  document.querySelector('.read-more-conta');
+parentContainer5.addEventListener('click', event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read less..." : "Read More...";
+})
+const parentContainer6 =  document.querySelector('.read-more-contai');
+parentContainer6.addEventListener('click', event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read less..." : "Read More...";
+})
+const parentContainer7 =  document.querySelector('.read-more-contain');
+parentContainer7.addEventListener('click', event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read less..." : "Read More...";
+})
+const parentContainer8 =  document.querySelector('.read-more-containe');
+parentContainer8.addEventListener('click', event=>{
     const current = event.target;
     const isReadMoreBtn = current.className.includes('read-more-btn');
     if(!isReadMoreBtn) return;
